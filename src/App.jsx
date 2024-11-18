51cd1d048f5f354eae152092bfce1a64dfc2ad6a
Eva Sevillian <evasevillanop@gmail.com>
2024-11-18 14:43:42 +0100

    Logo added

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {}
+        <img
+          src="/ShutterGarden.png"
+          alt="ShutterGarden Logo"
+          className="logo"
+        />
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
+
