110ec9910c3956bc3dcfc7e169f682e4698f72a7
Eva Sevillian <evasevillanop@gmail.com>
2024-12-03 18:26:54 +0100

    Gallery update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -5,7 +5,7 @@ export const Gallery = ({ images, deleteButton, downloadButton, favButton }) => 
   <div className="gallery">
     {images.map((image) => (
       <div key={image.id} className="image-container">
-        <img src={image.urls.small} alt={image.alt_description} />
+        <img src={image.urls.regular} alt={image.alt_description} />
         <div className="buttons">
           {favButton}
           {deleteButton}
